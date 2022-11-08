5c542f79bc1667a36c31845a669d4e7a6918631a
Sharvani <kasharvani@deloitte.com>
2022-11-08 18:55:19 +0530

    initial commit

---
+++ DemoProject/DemoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Sharvani, Karrepu on 08/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
